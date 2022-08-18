5b140d968a6357c236171f13c1ee6faa6a2e7c07
elevenanger <550356416@qq.com>
2022-08-18 22:17:45 +0800

    pointer

--- ch2/str_pass.c
+++ ch2/str_pass.c
@@ -16,6 +16,51 @@ void hello_cookie(char msg[])
     printf("cookie 消息长度 : %lu\n", sizeof(msg));
 }
 
+/**
+ * @brief 数组变量不能指向其他地方
+ * 计算机会为数组分配空间
+ * 但是不会为数组变量分配空间
+ * 编译器仅在出现它的地方将它替换为数组的起始地址
+ * 但是由于计算机没有为数组变量分配空间
+ * 数组变量就不会指向其它地方
+ */
+void arr_var() {
+    int drinks[] = {1, 2, 3};
+    printf("数组变量的值: %p\n", drinks);
+    printf("数组变量的内存地址: %p\n", &drinks);
+    
+    /**
+     * drinks[0] 和 *drinks 是等价的
+     * 因为 drinks 的值为数组变量
+     * 是数组起始元素的内存地址
+     * *drinks 是内存地址的解引用，对应内存地址存储的元素
+     * drinks[i] = *(drinks + i)
+     */
+    printf("drinks[0] 的值 %i \n", drinks[0]);
+    printf("*drinks 的值 %i \n", *drinks);
+    printf("drinks[%i] 的值 %i , *(drinks + %i) 的值: %i\n", 
+            1, drinks[1], 1, *(drinks + 1));
+}
+
+void skip(char* msg)
+{
+    puts(msg + 6);
+}
+
+void pointer_arr() 
+{
+    /**
+     * @brief 不同类型指针变量进行算数运算的结果不同
+     * int 指针加 1 指针会指向下加上4的寄存器的地址
+     * 因为 int 类型占用 4 字节
+     */
+    int ints[] = {1, 2, 3};
+    printf("ints 中元素的地址 ints: %p ints + 1: %p ints + 2: %p",
+                            ints, ints + 1, ints + 2);
+    long longs[] = {1, 2, 3};
+    printf("ints 中元素的地址 longs: %p longs + 1: %p longs + 2: %p",
+                            longs, longs + 1, longs + 2);
+}
 int main()
 {
     char msg[] = "hello cookies";
@@ -44,5 +89,11 @@ int main()
         printf("msg[%i] 的值：%c, 内存地址：%p\n", i, msg[i], &msg[i]);
     }
     
+    arr_var();
+
+    char *msgs = "don't call me!";
+    skip(msgs);
+
+    pointer_arr();
     return 0;
 }
